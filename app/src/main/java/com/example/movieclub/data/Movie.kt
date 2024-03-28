b2aa9ca876ba7cf36b37e77a28fdfdd2903dd9b1
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-03-28 00:36:36 +0000

    feat: Implemented data class and string resources

---
+++ app/src/main/java/com/example/movieclub/data/Movie.kt
@@ -0,0 +1,16 @@
+package com.example.movieclub.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.movieclub.R
+
+data class Movie(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val movieName: Int,
+    @StringRes val movieDir: Int,
+    @StringRes val moviePlot: Int
+)
+
+val movies = listOf(
+    Movie(R.drawable., R.string.movie_1, R.string.movie_1_dir, R.string.movie_1_plot),
+)
