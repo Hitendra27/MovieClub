0f6edb889cc3d1e40b467474501e51b01969fa2d
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-01 21:49:39 +0100

    feat: Implemented Top App Bar

--- app/src/main/java/com/example/movieclub/MainActivity.kt
+++ app/src/main/java/com/example/movieclub/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
@@ -51,7 +57,11 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun MovieClubApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            MovieClubTopBar()
+        }
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(movies) {
                 MovieItem(
@@ -61,6 +71,35 @@ fun MovieClubApp() {
             }
         }
     }
+}
+
+/**
+ * Movie App Top Bar
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieClubTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size)),
+                    painter = painterResource(R.drawable.film),
+                    contentDescription = null
+                )
+                Text(text = stringResource(R.string.movie_topapp_bar),
+                    style = MaterialTheme.typography.displayLarge,
+                    modifier = modifier.padding(start = 8.dp)
+                )
+
+            }
+        },
+        modifier = modifier
+    )
+
 }
 
 /**
@@ -128,9 +167,13 @@ fun MovieInformation(
     Column(modifier = modifier) {
         Text(
             text = stringResource(movieName),
+            style = MaterialTheme.typography.displayMedium,
             modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
         )
-        Text(text = stringResource(movieDirector))
+        Text(
+            text = stringResource(movieDirector),
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 @Preview(showBackground = true)
--- app/src/main/java/com/example/movieclub/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieclub/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.example.movieclub.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
@@ -85,6 +86,7 @@ fun MovieClubTheme(
 
     MaterialTheme(
         colorScheme = colors,
-        content = content
+        content = content,
+        typography = Typography,
     )
 }
